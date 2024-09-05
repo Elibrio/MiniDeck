923318ade6216fdaf6ec54885efc5e1f2cfb4714
elibrio <syncelias@proton.me>
2024-09-05 17:17:09 +0200

    MiniDeck init! (R.I.P. UwUDeck)

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import javax.sound.midi.MidiUnavailableException;
+
+public class Main {
+
+    public static void main(String[] args) throws MidiUnavailableException {
+        //Only for forks
+        final MiniDeck deck = new MiniDeck();
+    }
+
+}
